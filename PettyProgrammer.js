78eb5c21ba5c0041debcdc60e6c5e2815e28a4d5
Manmaya Rama K A <105375195+Manmayarama@users.noreply.github.com>
2023-10-03 20:40:03 +0530

    Update PettyProgrammer.js

--- PettyProgrammer.js
+++ PettyProgrammer.js
@@ -85,3 +85,39 @@ document.querySelector('#btn_re').addEventListener('click',() =>{
   document.querySelector('#eight-gpa').value='';
 })
 // onclick Event function call reset all input and output value
+
+// To disable right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// To disable F12 options
+document.onkeypress = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+document.onmousedown = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+document.onkeydown = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+
+// To Disable ctrl+c, ctrl+u
+jQuery(document).ready(function($){
+  $(document).keydown(function(event) {
+    var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+      alert('Sorry, This Functionality Has Been Disabled!');
+      //disable key press processing
+      return false;
+    }
+  });
+});
